79a040a87f408508be5a306460f33558b0c1925a
MathiasChirivi <chirivimathias3@gmail.com>
2023-04-21 14:41:50 +0200

    parola palindroma finito

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+let wordl = prompt("Inserisci una parola");
+
+let parolaInversa = invertiParola(wordl);
+
+if (wordl == parolaInversa) {
+    console.log("La parola :", wordl ,"è palindroma")
+} else {
+    console.log("La parola :", wordl ,"non è palindroma");
+}
+
+function invertiParola(str) {
+    var strInversa = str.split('').reverse().join('');
+    return strInversa;
+}
