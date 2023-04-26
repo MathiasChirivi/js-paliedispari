ab02312661cbae21d8411c1e68ad151784e137b8
MathiasChirivi <chirivimathias3@gmail.com>
2023-04-26 10:55:03 +0200

    fix and comment es.

--- js/main.js
+++ js/main.js
@@ -1,61 +1,90 @@
- // Chiedere all’utente di inserire una parola
- // Creare una funzione per capire se la parola inserita è palindrom
+/******************
+**PALINDROMA*******/
 
- // let wordl = prompt("Inserisci una parola");
- // 
- // let parolaInversa = invertiParola(wordl);
- // console.log(parolaInversa);
- // 
- // if (wordl == parolaInversa) {
- //     console.log("La parola :", wordl ,"è palindroma")
- // } else {
- //     console.log("La parola :", wordl ,"non è palindroma");
- // }
- // 
- // function invertiParola(Parola) {
- //   var parolaReverse = '';
- //   for (var i = Parola.length - 1; i >=0; i--){ 
- //         parolaReverse += Parola [i]; 
- //         }  
- //         return parolaReverse;  
- //        }
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindrom
+
+//let wordl = prompt("Inserisci una parola");
+//
+//let parolaInversa = invertiParola(wordl);
+//console.log(parolaInversa);
+//
+//if (wordl == parolaInversa) {
+//    console.log("La parola :", wordl ,"è palindroma")
+//} else {
+//    console.log("La parola :", wordl ,"non è palindroma");
+//}
+//
+//function invertiParola(Parola) {
+//  var parolaReverse = '';
+//  for (var i = Parola.length - 1; i >=0; i--){ 
+//        parolaReverse += Parola [i]; 
+//        }  
+//        return parolaReverse;  
+//      }    
+//
+/****************************** 
+***ESERCIZIO PARI DISPARI******/
 
 
 
-let pariOrDispari = prompt("Pari o dispari?");
+//chiedere pari o dispari all'utonto
+
+let pariOrDispari = prompt("Pari o Dispari?");
 console.log("Hai scelto", pariOrDispari);
 
-let number = parseInt(prompt("Scegli un numero da 1 a 5"));
-console.log("Hai scelto il numero", number);
+//chiedere un numero da 1 a 5 all'utonto
+let inpututente = false;
+let number;
 
+while (inpututente == false){
+  number = parseInt(prompt("Scegli un numero da 1 a 5"));
+
+  if(isNaN(number)==false && number>=1 && number<=5){
+    inpututente = true;
+  }else {
+    alert("Inserisci un numero tra 1 e 5");
+  }
+}
+
+//funzione di un numero dandom da 1 a 5
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// funziopne somma tra numero scelto dall'utonto e quello generato random
 function somma() {
     var somma = number + rand;
     return somma;
 }
 
-function checkPariDispari(numero) {
+//funzione Pari o Dispari
+function checkPariDispari(somma) {
     if (somma % 2 == 0) {
       console.log("Pari")
       return "Pari";
-    }
-      console.log("Pari")
+    } else{
+      console.log("Dispari")
       return "Dispari";
     }
-    
+  }
+
+//funzione di un numero dandom da 1 a 5
 var rand = random(1, 5);
 console.log("Numero generato per l'avversario è : " , rand);
 
+// funziopne somma tra numero scelto dall'utonto e quello generato random
 var risultato = somma();
 console.log("La somma dei due numeri è :", risultato);
 
+//Risultato
 var result = checkPariDispari(risultato);
 
-if (pariOrDispari === result) {
+//controllo se l'utonto ha vinto o meno
+if (pariOrDispari == result) {
     console.log("vince il giocatore");
+    alert("Hai vinto, complimenti");
   } else {
     console.log("vince il pc");
+    alert("Hai perso, ritenta sarai piu fortunato");
   }
